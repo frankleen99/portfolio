8d0b6d85fba34b6cf24a81f9640ee43ee851aaa0
AFranklyn <akanofranklyn@gmail.com>
2025-08-01 08:10:16 +0100

    fixed issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import About from "./Components/About"
 import Contact from "./Components/Contact"
 import Experience from "./Components/Experience"
 import Home from "./Components/Home"
-import NavBar from "./components/NavBar"
+import NavBar from "./Components/NavBar"
 import Portfolio from "./Components/Portfolio"
 import SocialLinks from "./Components/SocialLinks"
 
